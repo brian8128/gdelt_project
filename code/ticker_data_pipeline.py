14ebdd2ef427f2a2cf0dc959ec78e59395567d52
Brian Bargh <brian.bargh@gmail.com>
2015-12-17 16:09:57 -0800

    Add function to get dataframe from flat files to ticker_data_pipeline

--- code/ticker_data_pipeline.py
+++ code/ticker_data_pipeline.py
@@ -2,6 +2,8 @@ from code.CONSTANTS import PROJECT_HOME
 
 import datetime
 import calendar
+import pandas as pd
+import os
 
 
 def get_timestamp(s):
@@ -12,4 +14,62 @@ def get_timestamp(s):
     d = datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
     return calendar.timegm(d.timetuple())
 
+
 def unzip():
+    pass
+
+
+def clean_series(s, min_timestamp=1433000000, min_mean_price_dollars=10):
+    '''
+    :param s: the series to clean
+    :param min_timestamp: cut off all values before this time
+    :param min_mean_price_dollars: return none if the mean price is less than this
+    :return: The cleaned series or None if the series is rejected
+    '''
+    pass
+
+
+def get_df_from_files(limit=100,
+                      download_month='201512',
+                      freq='hourly',
+                      region='us',
+                      market='nasdaq stocks'
+                      ):
+
+    path = [PROJECT_HOME,
+            'data/stooq',
+            download_month,
+            'data/',
+            freq]
+    if region is not None:
+        path.append(region)
+
+    if market is not None:
+        path.append(market)
+
+    g = os.walk('/'.join(path))
+    count = 0
+    series = []
+    for triple in g:
+        for filename in triple[2]:
+            try:
+                symbol = filename.split('.')[0]
+                path = '/'.join([triple[0], filename])
+
+                df = pd.read_csv(path)
+                df['timestamp'] = (df['Date'] + 'T' + df['Time']).apply(get_timestamp)
+                df = df.set_index(['timestamp'])
+                df[symbol] = df.Open
+                series.append(df[symbol])
+            except Exception:
+                # It seems that this is rare.  Not part of MVP if important at all.
+                pass
+            count += 1
+
+            # TODO: Guido says I should refactor this.  But how?
+            if count >= limit:
+                break;
+        if count >= limit:
+            break;
+
+    return pd.DataFrame(data=series).T
